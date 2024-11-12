fd4b434fb3cfa32267ae44eb64a645d5a20a3bc9
makaricheva liza <makarichevalm@yandex.ru>
2024-11-12 21:45:00 +0300

    add slider buttons

--- Task_02/slider/slider.js
+++ Task_02/slider/slider.js
@@ -1,9 +1,12 @@
 const baseURL = "https://jsonplaceholder.typicode.com/photos";
 const fetchUsers = () => {
-  axios.get(baseURL + "?_limit=15").then((response) => {
-    const photos = response.data;
-    console.log(`GET list users`, photos);
-  });
+  axios
+    .get(baseURL + "?_limit=15")
+    .then((response) => {
+      const photos = response.data;
+      console.log(`GET list users`, photos);
+    })
+    .catch((error) => console.error(error));
 };
 
 fetchUsers();
