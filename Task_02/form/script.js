df3bf03850a38caca99816f32158f9b5f0289ca3
makaricheva liza <makarichevalm@yandex.ru>
2024-11-13 20:17:20 +0300

    form is done

---
+++ Task_02/form/script.js
@@ -0,0 +1,33 @@
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+function showValid(input) {
+  input.className = "validInp";
+  let err = input.nextElementSibling;
+  err.innerText = "";
+}
+function showInvalid(input, mes) {
+  let err = input.nextElementSibling;
+  err.innerText = mes;
+  input.className = "invalidInp";
+}
+function checkUsername(input) {
+  const nameRegex = /^[a-zA-Zа-яА-Я\s]+$/;
+  if (input.value === "") showInvalid(input, "Please fill out this field");
+  else if (nameRegex.test(input.value)) showValid(input);
+  else showInvalid(input, "Use only letters");
+}
+function checkPassword(input) {
+  const passwordRegex =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+?]).{8,}$/;
+  if (input.value === "") showInvalid(input, "Please fill out this field");
+  else if (passwordRegex.test(input.value)) showValid(input);
+  else
+    showInvalid(
+      input,
+      "Use minimum 8 characters including letters, numbers and special characters exept -*/|_"
+    );
+}
+document.querySelector("#btnNext").addEventListener("click", function () {
+  checkUsername(username);
+  checkPassword(password);
+});
