0a997a9e9b06088de9f29eb0ac68c4d6c389586b
makaricheva liza <makarichevalm@yandex.ru>
2024-11-14 22:26:40 +0300

    add img in slider

--- Task_02/slider/slider.js
+++ Task_02/slider/slider.js
@@ -1,12 +1,28 @@
-const baseURL = "https://jsonplaceholder.typicode.com/photos";
-const fetchUsers = () => {
+const baseURL = "https://api.slingacademy.com/v1/sample-data/photos";
+const getPhotos = () => {
   axios
-    .get(baseURL + "?_limit=15")
+    .get(baseURL + "?limit=5")
     .then((response) => {
-      const photos = response.data;
+      const photos = response.data.photos;
       console.log(`GET list users`, photos);
+      showPhotos(photos);
     })
     .catch((error) => console.error(error));
 };
+const slider = document.querySelector(".slider_content");
 
-fetchUsers();
+function showPhotos(photos) {
+  photos.forEach((ph) => {
+    let block = document.createElement("div");
+    block.setAttribute("class", "slider_block");
+    slider.appendChild(block);
+    let img = document.createElement("img");
+    img.className = "slider_img";
+    img.src = ph.url;
+    block.appendChild(img);
+    let text = document.createElement("p");
+    text.textContent = ph.description;
+    block.appendChild(text);
+  });
+}
+getPhotos();
